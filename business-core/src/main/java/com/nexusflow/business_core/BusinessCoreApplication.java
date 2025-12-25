52da81bb1eeff68cba635cd18cd23efaf22152e6
wagnerdalpiccol <wdpiccol2005@gmail.com>
2025-12-25 18:57:53 -0300

    feat: initial project structure

---
+++ business-core/src/main/java/com/nexusflow/business_core/BusinessCoreApplication.java
@@ -0,0 +1,13 @@
+package com.nexusflow.business_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCoreApplication.class, args);
+	}
+
+}
---
+++ business-core/src/test/java/com/nexusflow/business_core/BusinessCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusflow.business_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
